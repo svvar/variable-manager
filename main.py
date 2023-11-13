574c0eeb25eb18a2bfac31824d6ddf6631e45717
svvar <aleksseva003@gmail.com>
2023-11-13 19:11:50 +0200

    v0.4 -Огляд каталога -Огляд файлів -Зберігання змінних після перезапуску

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
-import enum
-import os, sys
+import os
+import sys
 
 import PyQt5
-from PyQt5.QtCore import QStringListModel, QObject, pyqtSignal
-from PyQt5.QtWidgets import QApplication, QMainWindow, QTableWidgetItem, QFileDialog, QMessageBox
+from dotenv import load_dotenv
+from PyQt5 import QtGui
+from PyQt5.QtCore import QStringListModel
+from PyQt5.QtWidgets import QApplication, QMainWindow, QTableWidgetItem, QFileDialog
 
-import createWindow, mainWindow, EditWindow
-from PyQt5 import QtCore, QtGui, QtWidgets
+import EditWindow
+import createWindow
+import mainWindow
 
 
 class VariableManagerMain(QMainWindow):
@@ -14,10 +17,6 @@ class VariableManagerMain(QMainWindow):
         super().__init__()
 
         self.main_ui = mainWindow.Ui_mainWindow()
-
-        self.edit_window = VariableManagerEdit()
-        self.creation_window = VariableManagerCreate()
-        self.creation_window.creation_window_closed.connect(self.load_data)
 
         self.main_ui.setupUi(self)
 
@@ -30,7 +29,7 @@ class VariableManagerMain(QMainWindow):
         self.main_ui.pushButton_3.clicked.connect(self.enter_create_mode)
 
     def load_data(self):
-        data = dict(os.environ)
+        data = get_variables()
         table = self.main_ui.tableWidget
         table.setRowCount(len(data))
 
@@ -60,24 +59,31 @@ class VariableManagerMain(QMainWindow):
         self.main_ui.pushButton_2.setEnabled(True)
 
     def enter_edit_mode(self):
+        selected_variable = self.main_ui.tableWidget.selectedItems()[0].text()
         selected_variable_content = self.main_ui.tableWidget.selectedItems()[1].text()
         variable_values = selected_variable_content.split(';')
 
+        self.edit_window = VariableManagerEdit(selected_variable)
         self.edit_window.edit_ui.tableWidget.setRowCount(len(variable_values))
 
         # деякі значення можуть повторюватися у змінній, тому ітеруємо незвичним на перший погляд чином
         for n in range(len(variable_values)):
             self.edit_window.edit_ui.tableWidget.setItem(n, 0, QTableWidgetItem(variable_values[n]))
+
+        self.edit_window.edit_window_closed.connect(self.load_data)
         self.edit_window.show()
 
     def enter_create_mode(self):
+        self.creation_window = VariableManagerCreate()
+
+        self.creation_window.creation_window_closed.connect(self.load_data)
+
         self.creation_window.show()
 
     def delete(self):
-        item = self.main_ui.tableWidget.selectedItems()[0]
-        row = item.row()
+        name = self.main_ui.tableWidget.selectedItems()[0].text()
 
-        del os.environ[item.text()]
+        delete_variable(name)
 
         self.load_data()
         self.main_ui.tableWidget.clearSelection()
@@ -92,8 +98,11 @@ class VariableManagerMain(QMainWindow):
 
 
 class VariableManagerEdit(QMainWindow):
-    def __init__(self):
+    edit_window_closed = PyQt5.QtCore.pyqtSignal()
+
+    def __init__(self, variable):
         super().__init__()
+        self.variable = variable
 
         self.edit_ui = EditWindow.Ui_EditWindow()
         self.edit_ui.setupUi(self)
@@ -139,7 +148,6 @@ class VariableManagerEdit(QMainWindow):
         folder_path = QFileDialog.getExistingDirectory(self, "Select Folder", options=options)
 
     def delete(self):
-        # TODO Make it actually delete
         item = self.edit_ui.tableWidget.selectedItems()[0]
         row = item.row()
         while row < self.edit_ui.tableWidget.rowCount() - 1:
@@ -177,8 +185,7 @@ class VariableManagerEdit(QMainWindow):
             if text != "" and not text.isspace():
                 values.append(item.text())
 
-        for row in values:
-            print(row)
+        write_variable(self.variable, ';'.join(values))
 
         # TODO REWRITE VARIABLE HERE
 
@@ -190,6 +197,10 @@ class VariableManagerEdit(QMainWindow):
 
         self.edit_ui.tableWidget.setItem(row1, 0, item2)
         self.edit_ui.tableWidget.setItem(row2, 0, item1)
+
+    def closeEvent(self, a0: QtGui.QCloseEvent) -> None:
+        self.edit_window_closed.emit()
+        super().closeEvent(a0)
 
 
 class VariableManagerCreate(QMainWindow):
@@ -205,8 +216,8 @@ class VariableManagerCreate(QMainWindow):
         self.create_ui.lineEdit.textChanged.connect(self.update_button)
         self.create_ui.lineEdit_2.textChanged.connect(self.update_button)
         self.create_ui.pushButton.clicked.connect(self.save_variable)
-        # self.create_ui.pushButton_2.clicked.connect(pass)
-        # self.create_ui.pushButton_3.connect(self.browse)
+        self.create_ui.pushButton_2.clicked.connect(self.browse_folder)
+        self.create_ui.pushButton_3.clicked.connect(self.browse_files)
         self.create_ui.pushButton_4.clicked.connect(self.close)
 
     def update_button(self):
@@ -222,16 +233,95 @@ class VariableManagerCreate(QMainWindow):
         name = self.create_ui.lineEdit.text()
         value = self.create_ui.lineEdit_2.text()
 
-        os.environ[name] = value
+        write_variable(name, value)
         self.close()
 
+    def browse_folder(self):
+        options = QFileDialog.Options()
+        folder_path = QFileDialog.getExistingDirectory(self, "Вибір каталога", options=options)
+
+        if folder_path:
+            self.create_ui.lineEdit_2.setText(folder_path)
+
+    def browse_files(self):
+        options = QFileDialog.Options()
+        file_path = QFileDialog.getOpenFileName(self, "Вибір файлу", options=options)[0]
+
+        if file_path:
+            self.create_ui.lineEdit_2.setText(file_path)
+
     def closeEvent(self, a0: QtGui.QCloseEvent) -> None:
+        self.create_ui.lineEdit.clear()
+        self.create_ui.lineEdit_2.clear()
+
         self.creation_window_closed.emit()
         super().closeEvent(a0)
 
+def get_variables():
+    f = open('.env')
+    file_data = f.readlines()
+    data = {}
+    f.close()
+
+    for f in file_data:
+        v = f.split(sep='=', maxsplit=1)
+        data[v[0]] = v[1].strip()
+
+    return data
+
+
+def write_variable(name, value):
+    f = open('.env')
+    data = f.readlines()
+    f.close()
+    new_data = ""
+    edited = False
+
+    for d in data:
+        if f'{name}=' in d:
+            d = f'{name}={value}\n'
+            edited = True
+        new_data += d
+
+    if not edited:
+        new_data += f'{name}={value}\n'
+
+    f = open('.env', 'w')
+    f.write(new_data)
+    f.close()
+
+
+def delete_variable(name):
+    print(name)
+    f = open('.env')
+    data = f.readlines()
+    f.close()
+    new_data = ""
+
+    for d in data:
+        if f'{name}=' in d:
+            pass
+        else:
+            new_data += d
+
+    print(new_data)
+
+    f = open('.env', 'w')
+    f.write(new_data)
+    f.close()
+
 
 if __name__ == '__main__':
+    if not os.path.exists('.env'):
+        f = open('.env', 'x')
+        for k, v in dict(os.environ).items():
+            f.write(f'{k}={v}\n')
+        f.close()
+
     app = QApplication(sys.argv)
     window = VariableManagerMain()
     window.show()
     sys.exit(app.exec_())
+
+
+
